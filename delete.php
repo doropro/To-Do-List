eb9f74ea16589026766a51e39e5fbab2b3792568
doropro <weiss.dorothee.pro@gmail.com>
2019-03-23 21:53:39 +0100

    initialisation

---
+++ delete.php
@@ -0,0 +1,16 @@
+<?php
+///////////////////////////CONNEXION BDD
+$bdd = new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'phpmyadmin','user');
+//////////////////////////EFFACE TACHES
+if(isset($_GET['ID_Taches'])){//Si get id tache dans url
+$resultat = $bdd->prepare('DELETE FROM Todos WHERE ID_Taches=:ID_Taches');
+$resultat->execute(array(
+'ID_Taches'=> $_GET['ID_Taches']
+));
+///////////////////////redirige vers la page index.php
+header("location:index.php");
+
+
+     }
+
+?>
